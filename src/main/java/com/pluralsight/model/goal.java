76e1e564b9be35f425f01fc1912d8612040cabbb
Giorgi <giorgi.bibilashvili89@gmail.com>
2014-11-22 15:31:51 +0400

    add Goal Functionality

---
+++ src/main/java/com/pluralsight/model/goal.java
@@ -0,0 +1,14 @@
+package com.pluralsight.model;
+
+public class goal {
+	private String minutes;
+
+	public String getMinutes() {
+		return minutes;
+	}
+
+	public void setMinutes(String minutes) {
+		this.minutes = minutes;
+	}
+	
+}
