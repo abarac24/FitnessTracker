0d0a9024d40cccb62a6038f6432c2b5d390794af
Giorgi <giorgi.bibilashvili89@gmail.com>
2014-12-01 01:41:25 +0400

    Create Simple Goal Entity Manager

--- src/main/java/com/pluralsight/model/goal.java
+++ src/main/java/com/pluralsight/model/goal.java
@@ -1,14 +1,31 @@
 package com.pluralsight.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import org.hibernate.validator.constraints.Range;
 
+@Entity
 public class goal {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
 	
 	@Range(min=1, max=120)
 	private String minutes;
+	
+	public Long getId() {
+		return id;
+	}
 
 	public String getMinutes() {
 		return minutes;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public void setMinutes(String minutes) {
