8af238afb5d8f741ecb4a8ee4905f5dcb3af3774
Giorgi <giorgi.bibilashvili89@gmail.com>
2014-12-02 00:46:51 +0400

    Initialize and manipulate entity

--- src/main/java/com/pluralsight/model/goal.java
+++ src/main/java/com/pluralsight/model/goal.java
@@ -1,19 +1,23 @@
 package com.pluralsight.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
+import javax.persistence.Table;
 import org.hibernate.validator.constraints.Range;
 
+
 @Entity
-public class goal {
+@Table(name="goals")
+public class goal{
 	
 	@Id
 	@GeneratedValue
 	private Long id;
 	
 	@Range(min=1, max=120)
+	@Column(name="minutes")
 	private String minutes;
 	
 	public Long getId() {
