65844b9231210db78cce673b5a5845ca97c3daa9
Giorgi <giorgi.bibilashvili89@gmail.com>
2014-12-21 11:21:05 +0400

    Fix Session Warning

--- src/main/java/com/pluralsight/controller/GoalController.java
+++ src/main/java/com/pluralsight/controller/GoalController.java
@@ -1,9 +1,6 @@
 package com.pluralsight.controller;
 
 import javax.validation.Valid;
-import javax.websocket.Session;
-import javax.xml.ws.Service.Mode;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
